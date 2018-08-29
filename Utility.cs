ad48e41c0c9e3b01cc7a36c4903320c4906e0ebc
Piotr Paziak <paziakpiotr@gmail.com>
2018-08-29 21:28:10 +0200

    Utility TrimPath fix

--- Utility.cs
+++ Utility.cs
@@ -11,14 +11,8 @@ namespace Media_Player
         
         public static string TrimPath(string path)
         {
-            //char[] mychars = { '/' };
-
-            //path = path.Replace(@"%", "");
-            //path = path.Replace(@"2", "");
             path = path.Replace(@"%20", " ");
-
             int foundS1 = path.LastIndexOf("/");
-
             path = path.Remove(0, foundS1 + 1);
 
             return path;
